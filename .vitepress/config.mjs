dc17f6a8fbdb8d4c371a868958d75e730ce6a599
Pavel <sam0kat4ik@mail.ru>
2026-04-28 21:37:01 +0500

    Добавлены места, переделана главная страница, изменен дизайн сайта с обычной темы

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,28 +1,79 @@
 import { defineConfig } from 'vitepress'
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "knowledge base",
+  title: "Проектопакалипсис",
   description: "A VitePress Site",
+  lang: 'ru-RU',
+
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Главная', link: '/' },
+      { text: 'Места', link: '/places/' },
+      { text: 'Люди', link: '/people/' },
+      { text: 'Группы', link: '/bands/' },
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+    sidebar: {
+      '/places/': [
+        {
+          text: 'Места',
+          collapsed: false,
+          items: [
+            {
+              text: 'Все места',
+              link: '/places/',
+              collapsed: false,
+              items: [
+                {
+                  text: 'Центральные площадки',
+                  collapsed: false,
+                  items: [
+                    { text: 'ДК имени Свердлова', link: '/places/dk-sverdlova' },
+                    { text: 'Дворец молодёжи', link: '/places/dvorets-molodezhi' },
+                    { text: 'ДК Автомобилистов', link: '/places/dk-avtomobilistov' },
+                  ]
+                },
+                {
+                  text: 'Университеты и студенческие места',
+                  collapsed: false,
+                  items: [
+                    { text: 'УрФУ, Радиофак / РТФ УПИ', link: '/places/urfu-rtf' },
+                    { text: 'Общежитие № 11 УрФУ', link: '/places/obschezhitie-11-urfu' },
+                    { text: 'УрГАХУ, корпус «Пряник»', link: '/places/pryanik-urgahu' },
+                  ]
+                },
+                {
+                  text: 'Школы и культурные точки',
+                  collapsed: false,
+                  items: [
+                    { text: 'ДК МЖК / Центр культуры «Молодёжный»', link: '/places/dk-mzhk-molodezhny' },
+                    { text: 'Школа № 36', link: '/places/school-36' },
+                    { text: 'Школа № 2', link: '/places/school-2' },
+                  ]
+                },
+                {
+                  text: 'Личные адреса',
+                  collapsed: false,
+                  items: [
+                    { text: 'Дом Александра Башлачёва', link: '/places/bashlachev-house' },
+                  ]
+                }
+
+              ]
+            }
+          ]
+        }
+      ]
+    },
+
+    outline: {
+      level: 'deep',
+      label: 'Содержание страницы'
+    },
   }
 })
