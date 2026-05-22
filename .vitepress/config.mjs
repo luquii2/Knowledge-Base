6ff1dc037404f015662809c22c946dac8a3cd6a6
Pavel <sam0kat4ik@mail.ru>
2026-05-23 04:10:44 +0500

    Добавлены страницы групп и людей

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vitepress'
 
+const BASE = process.env.VITEPRESS_BASE ?? (
+    process.env.NODE_ENV === 'production' ? '/Knowledge-Base/' : '/'
+)
+
 export default defineConfig({
   title: "Проектопакалипсис",
   description: "A VitePress Site",
   lang: 'ru-RU',
-  base: '/Knowledge-Base/',
+  base: BASE,
 
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', { rel: 'icon', type: 'image/x-icon', href: `${BASE}favicon.ico` }],
+    ['link', { rel: 'shortcut icon', href: `${BASE}favicon.ico` }]
   ],
   ignoreDeadLinks: true,
 
@@ -18,7 +23,6 @@ export default defineConfig({
       { text: 'Люди', link: '/people/' },
       { text: 'Группы', link: '/bands/' },
     ],
-
 
     sidebar: {
       '/places/': [
@@ -65,9 +69,117 @@ export default defineConfig({
                     { text: 'Дом Александра Башлачёва', link: '/places/bashlachev-house' },
                   ]
                 }
-
               ]
             }
+          ]
+        }
+      ],
+
+      '/people/': [
+        {
+          text: 'Люди',
+          collapsed: false,
+          items: [
+            { text: 'Все люди', link: '/people/' },
+            { text: 'Ключевые фигуры', collapsed: false, items: [
+                { text: 'Вячеслав Бутусов', link: '/people/vyacheslav-butusov' },
+                { text: 'Владимир Шахрин', link: '/people/vladimir-shahrin' },
+                { text: 'Владимир Бегунов', link: '/people/vladimir-begunov' },
+                { text: 'Вадим Самойлов', link: '/people/vadim-samoilov' },
+                { text: 'Глеб Самойлов', link: '/people/gleb-samoilov' },
+                { text: 'Александр Козлов', link: '/people/alexander-kozlov' },
+                { text: 'Настя Полева', link: '/people/nastya-poleva' },
+                { text: 'Александр Пантыкин', link: '/people/alexander-pantykin' },
+                { text: 'Илья Кормильцев', link: '/people/ilya-kormiltsev' },
+                { text: 'Егор Белкин', link: '/people/egor-belkin' },
+                { text: 'Александр Башлачёв', link: '/people/alexander-bashlachev' }
+              ] },
+            { text: 'Свердловский рок-клуб и классика', collapsed: false, items: [
+                { text: 'Дмитрий Умецкий', link: '/people/dmitry-umetsky' },
+                { text: 'Алексей Могилевский', link: '/people/alexey-mogilevsky' },
+                { text: 'Олег Решетников', link: '/people/oleg-reshetnikov' },
+                { text: 'Валерий Северин', link: '/people/valery-severin' },
+                { text: 'Вячеслав Двинин', link: '/people/vyacheslav-dvinin' },
+                { text: 'Пётр Май', link: '/people/petr-may' },
+                { text: 'Андрей Котов', link: '/people/andrey-kotov' },
+                { text: 'Евгений Кормильцев', link: '/people/evgeny-kormiltsev' },
+                { text: 'Владимир Назимов', link: '/people/vladimir-nazimov' },
+                { text: 'Игорь Гришенков', link: '/people/igor-grishenkov' },
+                { text: 'Михаил Симаков', link: '/people/mikhail-simakov' },
+                { text: 'Сергей Чернышёв', link: '/people/sergey-chernyshev' },
+                { text: 'Сергей Кондаков', link: '/people/sergey-kondakov' },
+                { text: 'Владимир Филиппов', link: '/people/vladimir-filippov' },
+                { text: 'Александр Каменецкий', link: '/people/alexander-kamenetsky' },
+                { text: 'Андрей Коняхин', link: '/people/andrey-konyakhin' },
+                { text: 'Андрей Голиков', link: '/people/andrey-golikov' }
+              ] },
+            { text: 'Ранняя сцена', collapsed: false, items: [
+                { text: 'Игорь Скрипкарь', link: '/people/igor-skripkar' },
+                { text: 'Михаил Перов', link: '/people/mikhail-perov' },
+                { text: 'Андрей Балашов', link: '/people/andrey-balashov' },
+                { text: 'Евгений Димов', link: '/people/evgeny-dimov' },
+                { text: 'Аркадий Застырец', link: '/people/arkady-zastyrets' },
+                { text: 'Виктор Резников', link: '/people/viktor-reznikov' },
+                { text: 'Олег Моисеев', link: '/people/oleg-moiseev' },
+                { text: 'Сергей Полухин', link: '/people/sergey-polukhin' },
+                { text: 'Сергей Долгополов', link: '/people/sergey-dolgopolov' }
+              ] },
+            { text: 'Новая екатеринбургская сцена', collapsed: false, items: [
+                { text: 'Сергей Бобунец', link: '/people/sergey-bobunets' },
+                { text: 'Константин Лекомцев', link: '/people/konstantin-lekomtsev' },
+                { text: 'Николай Ротов', link: '/people/nikolay-rotov' },
+                { text: 'Максим Митенков', link: '/people/maxim-mitenkov' },
+                { text: 'Евгений Гантимуров', link: '/people/evgeny-gantimurov' },
+                { text: 'Олег Гененфельд', link: '/people/oleg-genenfeld' },
+                { text: 'Саша Гагарин', link: '/people/sasha-gagarin' },
+                { text: 'Сергей Королёв', link: '/people/sergey-korolev' },
+                { text: 'Александра Кучерова', link: '/people/alexandra-kucherova' },
+                { text: 'Руслан Манин', link: '/people/ruslan-manin' },
+                { text: 'Евгений Пьянков', link: '/people/evgeny-pyankov' },
+                { text: 'Сергей Данилов', link: '/people/sergey-danilov' },
+                { text: 'Олег Ягодин', link: '/people/oleg-yagodin' },
+                { text: 'Юрий Облеухов', link: '/people/yury-obleukhov' },
+                { text: 'Владимир Коперник', link: '/people/vladimir-kopernik' },
+                { text: 'Василий Скородинский', link: '/people/vasily-skorodinsky' },
+                { text: 'Олег Кудрявцев', link: '/people/oleg-kudryavtsev' },
+                { text: 'Денис Михайлов', link: '/people/denis-mikhailov' }
+              ] },
+            { text: 'Организаторы, исследователи, кино', collapsed: false, items: [
+                { text: 'Евгений Горенбург', link: '/people/evgeny-gorenburg' },
+                { text: 'Алексей Карасюк', link: '/people/alexey-karasyuk' },
+                { text: 'Алексей Балабанов', link: '/people/alexey-balabanov' }
+              ] },
+          ]
+        }
+      ],
+
+      '/bands/': [
+        {
+          text: 'Группы',
+          collapsed: false,
+          items: [
+            { text: 'Все группы', link: '/bands/' },
+            { text: 'Классика Свердловского рок-клуба', collapsed: false, items: [
+                { text: 'Наутилус Помпилиус', link: '/bands/nautilus-pompilius' },
+                { text: 'Чайф', link: '/bands/chaif' },
+                { text: 'Агата Кристи', link: '/bands/agata-kristi' },
+                { text: 'Настя', link: '/bands/nastya' },
+                { text: 'Урфин Джюс', link: '/bands/urfin-dzhus' },
+                { text: 'Апрельский марш', link: '/bands/aprelsky-marsh' },
+                { text: 'Отражение', link: '/bands/otrazhenie' },
+              ] },
+            { text: 'Ранняя сцена до рок-клуба', collapsed: false, items: [
+                { text: 'Трек', link: '/bands/trek' },
+                { text: 'Р-Клуб', link: '/bands/r-club' },
+              ] },
+            { text: 'Новая екатеринбургская сцена', collapsed: false, items: [
+                { text: 'Смысловые Галлюцинации', link: '/bands/smyslovye-gallyutsinatsii' },
+                { text: 'Сансара', link: '/bands/sansara' },
+                { text: 'Курара', link: '/bands/kurara' },
+              ] },
+            { text: 'Современная российская сцена', collapsed: false, items: [
+                { text: 'Обе-Рек', link: '/bands/obe-rek' },
+              ] },
           ]
         }
       ]
---
+++ people/index.md
@@ -0,0 +1,92 @@
+---
+title: Люди
+description: "Люди уральского рока: музыканты, авторы, организаторы, исследователи и фигуры городской культурной среды."
+outline: deep
+---
+
+# Люди
+
+Раздел собирает персоналии, связанные с уральским роком, Свердловским рок-клубом, группами, площадками, фестивалями, студенческой средой и городской культурной памятью.
+
+## Ключевые фигуры
+
+| Человек | Основная связь |
+|---|---|
+| [Вячеслав Бутусов](/people/vyacheslav-butusov) | [Наутилус Помпилиус](/bands/nautilus-pompilius) |
+| [Владимир Шахрин](/people/vladimir-shahrin) | [Чайф](/bands/chaif) |
+| [Владимир Бегунов](/people/vladimir-begunov) | [Чайф](/bands/chaif) |
+| [Вадим Самойлов](/people/vadim-samoilov) | [Агата Кристи](/bands/agata-kristi) |
+| [Глеб Самойлов](/people/gleb-samoilov) | [Агата Кристи](/bands/agata-kristi) |
+| [Александр Козлов](/people/alexander-kozlov) | [Агата Кристи](/bands/agata-kristi) |
+| [Настя Полева](/people/nastya-poleva) | [Настя](/bands/nastya), [Трек](/bands/trek) |
+| [Александр Пантыкин](/people/alexander-pantykin) | [Урфин Джюс](/bands/urfin-dzhus) |
+| [Илья Кормильцев](/people/ilya-kormiltsev) | [Наутилус Помпилиус](/bands/nautilus-pompilius), [Настя](/bands/nastya), [Урфин Джюс](/bands/urfin-dzhus) |
+| [Егор Белкин](/people/egor-belkin) | [Наутилус Помпилиус](/bands/nautilus-pompilius), [Настя](/bands/nastya), [Урфин Джюс](/bands/urfin-dzhus), [Р-Клуб](/bands/r-club) |
+| [Александр Башлачёв](/people/alexander-bashlachev) | поэт, музыкант, автор песен |
+
+## Свердловский рок-клуб и классика
+
+| Человек | Основная связь |
+|---|---|
+| [Дмитрий Умецкий](/people/dmitry-umetsky) | [Наутилус Помпилиус](/bands/nautilus-pompilius) |
+| [Алексей Могилевский](/people/alexey-mogilevsky) | [Наутилус Помпилиус](/bands/nautilus-pompilius), [Урфин Джюс](/bands/urfin-dzhus) |
+| [Олег Решетников](/people/oleg-reshetnikov) | [Чайф](/bands/chaif) |
+| [Валерий Северин](/people/valery-severin) | [Чайф](/bands/chaif) |
+| [Вячеслав Двинин](/people/vyacheslav-dvinin) | [Чайф](/bands/chaif) |
+| [Пётр Май](/people/petr-may) | [Агата Кристи](/bands/agata-kristi) |
+| [Андрей Котов](/people/andrey-kotov) | [Агата Кристи](/bands/agata-kristi) |
+| [Евгений Кормильцев](/people/evgeny-kormiltsev) | [Настя](/bands/nastya), [Апрельский марш](/bands/aprelsky-marsh) |
+| [Владимир Назимов](/people/vladimir-nazimov) | [Урфин Джюс](/bands/urfin-dzhus), [Апрельский марш](/bands/aprelsky-marsh), [Р-Клуб](/bands/r-club) |
+| [Игорь Гришенков](/people/igor-grishenkov) | [Апрельский марш](/bands/aprelsky-marsh) |
+| [Михаил Симаков](/people/mikhail-simakov) | [Апрельский марш](/bands/aprelsky-marsh) |
+| [Сергей Чернышёв](/people/sergey-chernyshev) | [Апрельский марш](/bands/aprelsky-marsh) |
+| [Сергей Кондаков](/people/sergey-kondakov) | [Отражение](/bands/otrazhenie) |
+| [Владимир Филиппов](/people/vladimir-filippov) | [Отражение](/bands/otrazhenie) |
+| [Александр Каменецкий](/people/alexander-kamenetsky) | [Отражение](/bands/otrazhenie) |
+| [Андрей Коняхин](/people/andrey-konyakhin) | [Отражение](/bands/otrazhenie) |
+| [Андрей Голиков](/people/andrey-golikov) | [Отражение](/bands/otrazhenie) |
+
+## Ранняя сцена
+
+| Человек | Основная связь |
+|---|---|
+| [Игорь Скрипкарь](/people/igor-skripkar) | [Трек](/bands/trek) |
+| [Михаил Перов](/people/mikhail-perov) | [Трек](/bands/trek) |
+| [Андрей Балашов](/people/andrey-balashov) | [Трек](/bands/trek) |
+| [Евгений Димов](/people/evgeny-dimov) | [Трек](/bands/trek) |
+| [Аркадий Застырец](/people/arkady-zastyrets) | [Трек](/bands/trek) |
+| [Виктор Резников](/people/viktor-reznikov) | [Р-Клуб](/bands/r-club) |
+| [Олег Моисеев](/people/oleg-moiseev) | [Р-Клуб](/bands/r-club) |
+| [Сергей Полухин](/people/sergey-polukhin) | [Р-Клуб](/bands/r-club) |
+| [Сергей Долгополов](/people/sergey-dolgopolov) | [Р-Клуб](/bands/r-club) |
+
+## Новая екатеринбургская сцена
+
+| Человек | Основная связь |
+|---|---|
+| [Сергей Бобунец](/people/sergey-bobunets) | [Смысловые Галлюцинации](/bands/smyslovye-gallyutsinatsii) |
+| [Константин Лекомцев](/people/konstantin-lekomtsev) | [Смысловые Галлюцинации](/bands/smyslovye-gallyutsinatsii) |
+| [Николай Ротов](/people/nikolay-rotov) | [Смысловые Галлюцинации](/bands/smyslovye-gallyutsinatsii), [Курара](/bands/kurara) |
+| [Максим Митенков](/people/maxim-mitenkov) | [Смысловые Галлюцинации](/bands/smyslovye-gallyutsinatsii) |
+| [Евгений Гантимуров](/people/evgeny-gantimurov) | [Смысловые Галлюцинации](/bands/smyslovye-gallyutsinatsii) |
+| [Олег Гененфельд](/people/oleg-genenfeld) | [Смысловые Галлюцинации](/bands/smyslovye-gallyutsinatsii) |
+| [Саша Гагарин](/people/sasha-gagarin) | [Сансара](/bands/sansara) |
+| [Сергей Королёв](/people/sergey-korolev) | [Сансара](/bands/sansara) |
+| [Александра Кучерова](/people/alexandra-kucherova) | [Сансара](/bands/sansara) |
+| [Руслан Манин](/people/ruslan-manin) | [Сансара](/bands/sansara) |
+| [Евгений Пьянков](/people/evgeny-pyankov) | [Сансара](/bands/sansara) |
+| [Сергей Данилов](/people/sergey-danilov) | [Сансара](/bands/sansara) |
+| [Олег Ягодин](/people/oleg-yagodin) | [Курара](/bands/kurara) |
+| [Юрий Облеухов](/people/yury-obleukhov) | [Курара](/bands/kurara) |
+| [Владимир Коперник](/people/vladimir-kopernik) | [Курара](/bands/kurara) |
+| [Василий Скородинский](/people/vasily-skorodinsky) | [Курара](/bands/kurara) |
+| [Олег Кудрявцев](/people/oleg-kudryavtsev) | [Курара](/bands/kurara) |
+| [Денис Михайлов](/people/denis-mikhailov) | [Обе-Рек](/bands/obe-rek) |
+
+## Организаторы, исследователи, кино
+
+| Человек | Основная связь |
+|---|---|
+| [Евгений Горенбург](/people/evgeny-gorenburg) | организатор, первый директор Свердловского рок-клуба |
+| [Алексей Карасюк](/people/alexey-karasyuk) | историк уральского рока, автор и исследователь |
+| [Алексей Балабанов](/people/alexey-balabanov) | режиссёр, автор киноязыка, связанный со свердловским андеграундом |
